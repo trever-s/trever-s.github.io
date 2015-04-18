357b2e115a481a94f037eec3c03ccfbc743b836c
Trever Santora <trever@iron-point.com>
2015-04-17 23:47:50 -0700

    A thousand changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,52 @@
 module.exports = function(grunt) {
-	
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		sass: {
-			options: {
-				includePaths: ['bower_components/foundation/scss']
-			},
-			dist: {
-				options: {
-					outputStyle: 'compressed'
-				},
-				files: {
-					'css/styles.css': 'scss/styles.scss'
-				}
-			}
-		},
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'css/styles.css': 'scss/styles.scss'
+        }
+      }
+    },
 
-		shell: {
-				jekyllBuild: {
-					command: 'jekyll build'
-				}
-		},
+    shell: {
+        jekyllBuild: {
+          command: 'jekyll build'
+        }
+    },
 
-		connect: {
-			server: {
-				options: {
-					port: 4000,
-					base: '_site'
-				}
-			}
-		},
+
+// livereloadx: {
+//       static: true,
+//       dir: '.'
+//     },
+//     watch: {
+//       jekyll: {
+//         files: ['**/*.html', '**/*.md', '_layouts/*.html', 'scss/*.scss', 'css/*.css', 'scripts/*.js', 'images/*'],
+//         tasks: ['shell:jekyll_build']
+//       }
+//     },
+
+    connect: {
+      server: {
+        options: {
+          port: 4000,
+          base: '_site'
+        }
+      }
+    },
+
+
+
+
    uglify: {
      my_target: {
        files: {
@@ -46,54 +62,67 @@ module.exports = function(grunt) {
          ]
        }
      }
-   },		
-		watch: {
-			grunt: { files: ['Gruntfile.js'] },
-			sass: {
-				files: 'scss/**/*.scss',
-				tasks: ['sass']
-			},
-			uglify: {
-				files: 'js/*.js',
-				tasks: ['uglify']
-			},
-			livereload: {
-				files: [
-					'_config.yml',
-					'index.html',
-					'about-us/index.html',
-					'case-studies/index.html',
-					'contact-us/index.html',
-					'privacy-policy/index.html',
-					'_layouts/**',
-					'_posts/**',
-					'_includes/**',
-					'scss/**/*.scss',
-					'assets/**'
-				],
-				tasks: ['shell:jekyllBuild'],
-				options: {
-					livereload: true
-				}
-			}
-		}
-	});
+   },   
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
 
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+// all: {
+//             files: '**/*.html',
+//             options: {
+//                 livereload: true
+//         }
+//     },
 
-	grunt.registerTask('build', [
-		'sass',
-		'uglify'
-	]);
-	grunt.registerTask('default', [
-		'shell',
-		'build',
-		'connect',
-		'watch'
-	]);
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      },
+      uglify: {
+        files: 'js/*.js',
+        tasks: ['uglify']
+      },
+      livereload: {
+        files: [
+          '_config.yml',
+          'index.html',
+          'about-us/index.html',
+          'case-studies/index.html',
+          'contact-us/index.html',
+          'privacy-policy/index.html',
+          '_layouts/**',
+          '_posts/**',
+          '_includes/**',
+          'scss/**/*.scss',
+          'assets/**'
+        ],
+        tasks: ['shell:jekyllBuild'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('livereloadx');
+
+
+  grunt.registerTask('build', [
+    'sass',
+    'uglify'
+  ]);
+  grunt.registerTask('default', [
+    'shell',
+    'build',
+    // 'livereloadx',
+    'connect',
+    'watch'
+  ]);
 
 };
